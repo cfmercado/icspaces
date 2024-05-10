20987e1f383a55437a87e3543b0a1987319699b1
cfmercado <cfmercado@up.edu.ph>
2024-05-10 17:24:14 +0800

    REMOVE FUCKING SLASH

--- backend/backend/index.js
+++ backend/backend/index.js
@@ -32,7 +32,7 @@ app.use(express.json());
 
 //Allow CORS
 app.use(cors({
-    origin: 'https://app.icspaces.online/', //allowed URLs
+    origin: 'https://app.icspaces.online', //allowed URLs
     // methods: ['GET', 'POST', 'OPTIONS'], //allowed HTTP methods
     credentials: true, //allow cookies
     // allowedHeaders: ['Access-Control-Allow-Origin','Access-Control-Allow-Methods','Origin','Accept','Content-Type','X-Requested-With','Cookie']
